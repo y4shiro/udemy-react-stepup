212f03a4fb7043cf0e783a6955f9bd8a7cb457cc
y4shiro <y4shironao@gmail.com>
2021-03-09 11:20:57 +0900

    プロジェクトリセット

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import { useState, useCallback, useMemo } from "react";
-import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
-  console.log('App');
-  const [text, setText] = useState('');
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
-
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
-
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
