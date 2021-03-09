1bacf47f88e1cf7dec591ab21818a20364184954
y4shiro <y4shironao@gmail.com>
2021-03-09 20:01:14 +0900

    14. Styled JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
