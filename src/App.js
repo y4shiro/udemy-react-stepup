53ce66b99ac1f00308c32000cd075395ae3dafb0
y4shiro <y4shironao@gmail.com>
2021-03-09 19:50:53 +0900

    13. CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
