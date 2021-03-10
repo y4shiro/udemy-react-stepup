fbac9da9f711963e9d3245bf312c79a93bbb469d
y4shiro <y4shironao@gmail.com>
2021-03-10 22:22:58 +0900

    25. 404ページを用意する

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,8 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
+
+  const history = useHistory();
+
+  const onClickDetailA = () => history.push("/page1/detailA");
 
   return (
     <div>
@@ -10,6 +14,8 @@ export const Page1 = () => {
       <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
