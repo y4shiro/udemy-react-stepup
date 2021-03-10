9e06e3a8b0d73b7314a464bc5881861873abb1aa
y4shiro <y4shironao@gmail.com>
2021-03-10 11:51:52 +0900

    17. React Router導入&事前準備

---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home ページです</h1>
+    </div>
+  );
+};
---
+++ src/Page1.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <h1>Page1 ページです</h1>
+    </div>
+  );
+};
