56c3cff1d11ef600c14c477ec492be59c04d7625
y4shiro <y4shironao@gmail.com>
2021-03-11 18:22:29 +0900

    29. Organismの作成1

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>example@example.com</dd>
+        <dt>TEL</dt>
+        <dd>000-0000-0000</dd>
+        <dt>会社名</dt>
+        <dd>example会社</dd>
+        <dt>WEB</dt>
+        <dd>example.com</dd>
+      </dl>
+    </div>
+  );
+};
