a8effb5a62b8c528a8b469fe54a68da624cf0624
y4shiro <y4shironao@gmail.com>
2021-03-12 23:22:16 +0900

    37. Contextでのstate管理(ユーザー情報の設定と参照)

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { useLocation } from "react-router-dom";
 
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
@@ -20,17 +19,13 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
-
-  console.log(state);
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
       <SUserArea>
         {users.map((user) => (
-          <UserCard key={user.id} user={user} isAdmin={isAdmin}/>
+          <UserCard key={user.id} user={user}/>
         ))}
       </SUserArea>
     </SContainer>
