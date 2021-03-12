b6e283cb5a168927f257ae9e74ced4aeafdb6226
y4shiro <y4shironao@gmail.com>
2021-03-12 17:45:08 +0900

    32. Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
-
-const user = {
-  name: "なんとか太郎",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "example@example.com",
-  phone: "000-0000-0000",
-  company: {
-    name: "テスト株式会社",
-  },
-  website: "https://google.com"
-};
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
+    <Router />
   );
 }
