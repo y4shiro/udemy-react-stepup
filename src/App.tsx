8d6631a1ebf46447d8887da8b418a685beef7fd1
y4shiro <y4shironao@gmail.com>
2021-03-14 19:46:35 +0900

    プロジェクトリセット

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { Practice1 } from "./practices/Practice1";
-import { Practice2 } from "./practices/Practice2";
-import { Practice3 } from "./practices/Practice3";
-import { Practice4 } from "./practices/Practice4";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Practice1 />
-      <Practice2 />
-      <Practice3 />
-      <Practice4 />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
