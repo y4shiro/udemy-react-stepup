120593719bd9492ab23c1c423b626b87632279d2
y4shiro <y4shironao@gmail.com>
2021-03-14 16:43:59 +0900

    38. Contextでのstate管理(再レンダリングの最適化)

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 
+import { UserContext } from "../../provider/UserProvider";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 
@@ -19,10 +21,19 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const onClickSwitch = () => {
+    setUserInfo({ isAdmin: !userInfo.isAdmin });
+  };
+
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>
+        切り替え
+      </SecondaryButton>
       <SUserArea>
         {users.map((user) => (
           <UserCard key={user.id} user={user}/>
