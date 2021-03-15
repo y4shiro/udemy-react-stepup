1a49642e2894a525b5e0968d4641181a67edaddb
y4shiro <y4shironao@gmail.com>
2021-03-15 16:42:53 +0900

    55. オプショナルチェイニングでnull安全なコードを書く

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "太郎",
+  hobbies: ["映画", "ゲーム"],
+}
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -15,6 +22,7 @@ export default function App() {
   }
   return (
     <div className="App">
+      <UserProfile user={user}/>
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
