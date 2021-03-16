78575d080af87e78a445330c662d1396d012cf07
y4shiro <y4shironao@gmail.com>
2021-03-16 11:41:05 +0900

    プロジェクトリセット

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import "./styles.css";
-import { UserCard } from "./components/UserCard";
-import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const { getUsers, userProfiles, loading, error } = useAllUsers();
-
-  const onClickFetchUser = () => getUsers();
-
   return (
     <div className="App">
-      <button onClick={onClickFetchUser}>データ取得</button>
-      {error ? (
-        <p style={{ color: "red" }}>データの取得に失敗しました</p>
-      ) : loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {userProfiles.map((user) => (
-            <UserCard key={user.id} user={user} />
-          ))}
-        </>
-      )}
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
--- src/types/userProfile.ts
+++
@@ -1,6 +0,0 @@
-export type UserProfile = {
-  id: number;
-  name: string;
-  email: string;
-  address: string;
-}
