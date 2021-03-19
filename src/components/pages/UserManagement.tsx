5c4bf9b3d5208c027bce6705ef1dd585dee9dcf3
y4shiro <y4shironao@gmail.com>
2021-03-19 23:57:02 +0900

    74. ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,17 @@
+import { Wrap, WrapItem } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="フロントエンド太郎"
+          fullName="Frontend Taro"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
